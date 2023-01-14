dbeb96eec1a238b0940c232e892443e888c8249a
Yahiya17 <115204286+Yahiya17@users.noreply.github.com>
2023-01-14 10:59:53 +0530

    Create Binary_search.c

---
+++ Binary_search.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	int a[20],i,n,mid,l,h,k=0,f,m=0;
+	printf("Enter size of array ");
+	scanf("%d",&n);
+	l=0,h=n-1;
+	for(i=0;i<n;i++)
+	{
+		printf("Enter data ");
+		scanf("%d",&a[i]);
+	}
+
+	printf("\n Enter element to search \n");
+	scanf("%d",&f);
+	while(l<=h)
+	{
+		
+		printf("%d\n",m);
+		m++;
+		mid=(l+h)/2;
+		if(f==a[mid])
+		{
+			printf("Element is present ");
+			k++;
+			break;
+		}
+		else if(f>a[mid])
+		{
+			l=mid+1;
+		}
+		else if(f<a[mid])
+		{
+			h=mid-1;
+		}
+	}
+	if(k==0)
+	{
+		printf("%d is not present ",f);
+	}
+	getch();
+}
